2bf56aa3fb10c4b5c4605f6e4ad6a837fd214000
MarkelJM <markjuaristi@gmail.com>
2022-07-23 21:58:58 +0200

    feat: created information scene

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     game = TheQuest()
     game.play()
     
+    
--- thequest/scenes.py
+++ thequest/scenes.py
@@ -2,7 +2,7 @@ import os
 
 import pygame as pg
 
-from . import BACKGROUND_COLOUR, FPS, HEIGHT, LIFES,  MESSAGE_COLOUR,   WIDTH
+from . import BACKGROUND_COLOUR, FPS, HEIGHT, LIFES, MAIN_TEXT_SIZE,  MESSAGE_COLOUR, TEXT_MARGIN,  WIDTH
 
 
 class Scenes:
@@ -16,11 +16,17 @@ class Home(Scenes):
     def __init__(self, screen: pg.Surface):
         super().__init__(screen)
 
-        # self.logo = pg.image.load(
-        # os.path.join("resources", "images", "arkanoid_name.png"))
+        self.logo = pg.image.load(
+            os.path.join("resources", "images", "icon.png"))
 
-        #font_file = os.path.join("resources", "fonts", "CabinSketch-Bold.ttf")
-        #self.tipography = pg.font.Font(font_file, 40)
+        font_file = os.path.join("resources", "fonts", "CabinSketch-Bold.ttf")
+        self.tipography = pg.font.Font(font_file, MAIN_TEXT_SIZE)
+
+        self.message =  "'Space' to start playing"
+        self.information_text = "Push 'I' to read  game´s history and how to play"
+        self.message_pos =  0.75 * HEIGHT
+        self.information_pos = self.message_pos + MAIN_TEXT_SIZE + TEXT_MARGIN
+        
 
     def play(self):
         exit = False
@@ -29,6 +35,50 @@ class Home(Scenes):
             for event in pg.event.get():
                 if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                     exit = True
+                if event.type == pg.KEYDOWN and event.key == pg.K_i:
+                    exit = True
+                    #go to information scene
+                if event.type == pg.QUIT:
+                    pg.quit()
+
+            self.screen.fill(BACKGROUND_COLOUR)
+
+            self.draw_logo()
+            self.draw_text(self.message, self.message_pos)
+            self.draw_text(self.information_text, self.information_pos)
+
+            pg.display.flip()
+
+    def draw_logo(self):
+        width_logo = self.logo.get_width()
+        pos_x = (WIDTH - width_logo) / 2
+        pos_y = HEIGHT / 3
+        self.screen.blit(self.logo, (pos_x, pos_y))
+
+    def draw_text(self, text_input, sum_pos_y):
+        
+        text = self.tipography.render(text_input, True, MESSAGE_COLOUR)
+        width_text = text.get_width()
+        pos_x = (WIDTH - width_text) / 2     # ANCHO/2 - ancho_texto/2
+        
+        self.screen.blit(text, (pos_x, sum_pos_y))
+
+
+class Information(Scenes):
+    def __init__(self, screen: pg.Surface):
+        super().__init__(screen)        
+
+        font_file = os.path.join("resources", "fonts", "CabinSketch-Bold.ttf")
+        self.tipography = pg.font.Font(font_file, 20)
+
+    def play(self):
+        exit = False
+
+        while not exit:
+            for event in pg.event.get():
+                if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                    exit = True
+                
                 if event.type == pg.QUIT:
                     pg.quit()
 
